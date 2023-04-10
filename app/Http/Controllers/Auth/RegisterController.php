cc068486f3f16ad0aec68e34ecae334e5146e108
Abdessamad EL FEDALI <a.elfedali@gmail.com>
2023-04-10 13:22:55 +0000

    Register user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,47 +8,27 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function register (Request $request)
+    public function register(Request $request)
     {
-        try{
-         $fields = $request->validate([
-                'first_name' => 'required|string',
-                'last_name' => 'required|string',
-                'phone' => 'required|string|unique:profiles,phone',
-                'email' => 'required|string|unique:users,email',
-                "address" => "string",
-                "city" => "string",
-                "state" => "string",
-                "zip" => "integer",
-                "country" => "string",
-                'password' => 'required|string|confirmed'
-            ]);
-        }
-        catch(\Exception $e){
-            $errors = $e->validator->errors();
-            return response()->json([
-                'message' => 'Validation Error',
-                'errors' => $errors  
-            ], 400);
-        }
+
+        $fields = $request->validate([
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
 
         $user = User::create([
             'email' => $fields['email'],
             'password' => bcrypt($fields['password'])
         ]);
-        // create a profile for the user
-        $user->profile()->create($fields);
 
         $token = $user->createToken('myapptoken')->plainTextToken;
-         // user data its user and profile data
-         $userData = User::with('profile')->where('id', $user->id)->first();
+
         $response = [
-            'user' => $userData,
+            'user' => $user,
             'acces_token' => $token,
             'token_type' => 'Bearer'
         ];
 
         return response($response, 201);
     }
-   
 }
